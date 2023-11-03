b03566a7c306ff49e08b57603e0d0f40e251cd19
Brock Janssen <brockjanssen@ups.com>
2023-11-03 13:52:55 -0500

    Now identifies all winning conditions. Removed outdated comments

--- index.js
+++ index.js
@@ -1,51 +1,3 @@
-/* 
-
-    Build a grid 7 x 6
-        rid of positions: 7 col, 6 rows
-
-        We have a grid object, and each slot
-            Slot has info: 
-                Either: 0, Red, Black
-            CLASS OF SLOT -
-                Player: null
-                Coordinates/ [0,0] = bottom left (OR top left)
-            
-    Test for win condition:
-        Has to check all around itself
-        
-        Each current play/token play
-            Has 8 directions to check
-
-            //check all the way in one direction
-
-            //don't forget to also check in the other direction too
-
-    Build players
-        Name:
-        Color:
-        WinCounter:
-
-*/
-
-// set up the players
-
-
-// let currentPlayer = playerOne
-
-// const playerOne = {
-//     name : '',
-//     color: '',
-//     wins: 0
-
-// }
-
-// const playerTwo = {
-//     name: '',
-//     color: '',
-//     wins: 0
-
-// }
-
 // set up the board with a function 
 
 let instructions = document.getElementById('instructions');
@@ -62,7 +14,6 @@ class Game {
         this.board = [[],[],[],[],[],[],[]];
         this.activePlayer = 'red';
         this.makeBoard();
-        // this.activePlayer = 'red';
     }
 
     makeBoard() {
@@ -78,25 +29,15 @@ class Game {
                 let s = new Slot([c,r]);
                 let slot = document.createElement('div');
                 slot.addEventListener('click', () => {                    
-                    //place token in lowest available slot
                     this.placeToken(s);
-                    //change active player
-                    
-
-                    //check for win conditions
-                    //this.checkBoard();
                 })
-
                 slot.setAttribute('id',`${c}:${r}`);
                 slot.setAttribute('class','slot');
                 row.append(slot);
-                
                 this.board[c-1].push(s);
             }
-            
             board.append(row);
         }
-        
         instructions.innerText = `It is ${this.activePlayer}'s turn`
     }
 
@@ -116,21 +57,16 @@ class Game {
                 let filledSlot = document.getElementById(`${columnArray[i].coord[0]}:${columnArray[i].coord[1]}`);
                 if (this.activePlayer === 'red'){
                     filledSlot.classList.add('red');
-
                 } else {
                     filledSlot.classList.add('black');
-
                 }
                 let bottomSlot = [columnArray[0].coord[0], i + 1];
                 this.checkBoard(bottomSlot);
-                
-                
                 break;
             }
-            
-            
         }
 
+        //switch the current player
         this.activePlayer = this.activePlayer === 'red' ? 'black' : 'red';
         instructions.innerText = `It is now ${this.activePlayer}'s turn`
     }
@@ -147,22 +83,58 @@ class Game {
         for(let i = 0; i < 7; i++) {
             rowSlots.push(currentBoard[i][slot[1]-1]);
         }
+        this.checkLine(rowSlots);
 
         //create column array from new token position down to the bottom, slot[1]-1 to [5]
         for(let i = slot[1] - 1; i < 6; i++) {
             colSlots.push(currentBoard[slot[0]-1][i]);
         }
+        this.checkLine(colSlots);
 
         //create diagonal "up" array from bottom left to top right
-        //create diagonal "down" array from top left to bottom right
+        //start by finding bottom left slot
+        let bottomLeft = [slot[0], slot[1]];
+        while (bottomLeft[0] > 1 && bottomLeft[1] < 6) {
+            bottomLeft[0]--;
+            bottomLeft[1]++;
+        }
+        //push respective slots into the diagUpSlots array, moving up and right until edge of board is reached
+        let x = bottomLeft[0];
+        let y = bottomLeft[1];
+        while (x < 8 && y > 0){
+            diagUpSlots.push(currentBoard[x-1][y-1]);
+            x++;
+            y--;
+        }
+        this.checkLine(diagUpSlots);
 
-        //check the row array for win condition
+        //create diagonal "down" array from bottom right to top left
+        //start by finding bottom right slot
+        let bottomRight = [slot[0], slot[1]];
+        while (bottomRight[0] < 7 && bottomRight[1] < 6) {
+            bottomRight[0]++;
+            bottomRight[1]++;
+        }
+        
+        //push respective slots into the diagDownSlots array, moving up and left until edge of board is reached
+        x = bottomRight[0];
+        y = bottomRight[1];
+        while (x > 0 && y > 0){
+            diagDownSlots.push(currentBoard[x-1][y-1]);
+            x--;
+            y--;
+        }
+        console.log(diagDownSlots);
+        this.checkLine(diagDownSlots);
+    }
+
+    checkLine (line) {
         let consecutivePieces = 1;
-        for (let i = 1; i < rowSlots.length; i++) {
-            if (rowSlots[i].player && rowSlots[i].player === rowSlots[i-1].player){
+        for (let i = 1; i < line.length; i++) {
+            if (line[i].player && line[i].player === line[i-1].player){
                 consecutivePieces++;
                 if (consecutivePieces >=4) {
-                    console.log(rowSlots[i].player + " wins!");
+                    console.log(line[i].player + " wins!");
                     //add code here to add 1 to player's win counter and reset the board
                     return;
                 }
@@ -171,68 +143,13 @@ class Game {
                 consecutivePieces = 1;
             } 
         }
-
-        //check the column array for win condition
-        // consecutivePieces = 1;
-        // for (let i = 0; i < colSlots.length; i++) {
-        // //     
-        //     console.log(colSlots);
-        //     console.log(colSlots[i+1]);
-            
-        
-        //      if (colSlots[i].player && colSlots[i].player === colSlots[i+1].player){
-        //         consecutivePieces++;
-        //         if (consecutivePieces >=4) {
-        //             console.log(colSlots[i-1].player + " wins!");
-        //             return;
-        //         }
-        //     }
-        //     else {
-        //         consecutivePieces = 1;
-        //     } 
-        // }
-
-
-
-
-
-
     }
 }
 
-
 let game = new Game();
-// fill()
-//
 
 
-//What we know what we want to do
-//Play connect four
-//Have two players
-//Win counter
-//Have Slot classes that provide the player color and coordinates in the grid
-//What are the challenges we will face?
-//Getting our heads in the same place (having everyone understand the logic). Different perspectives.
-//Sufficiently communicating when we need help
+
 //add a dropping sound
 //yeet button
-
-// const grid = {
-//     slots: [],
-//     xValue: '',
-//     yValue: '',
-// }
-
-
-// function checkCoordinate() {
-    
-// }
-
-// class Slot {
-//     constructor(coords) {
-//         coords: [];
-//     }
-    
-
-
-
+//highlight the winning line
